17a4f503467d02bee75c8bd60c3d1cb759e85115
Trevor North <trevor@bugsnag.com>
2025-06-05 09:58:16 +0100

    Compatibility with GlusterFS 11 (#5)
    This is basically just to add OpenDir as Gluster 10.3+ no longer returns an error when opening a directory.

--- gfapi/volume.go
+++ gfapi/volume.go
@@ -81,10 +81,10 @@ func (v *Volume) InitWithVolfile(volname, volfile string) int {
 // Mount establishes a 'virtual mount.' Mount must be called after Init and
 // before storage operations. Steps taken:
 //
-//  - Spawn a poll-loop thread.
-//  - Establish connection to management daemon (volfile server) and receive volume specification (volfile).
-//  - Construct translator graph and initialize graph.
-//  - Wait for initialization (connecting to all bricks) to complete.
+//   - Spawn a poll-loop thread.
+//   - Establish connection to management daemon (volfile server) and receive volume specification (volfile).
+//   - Construct translator graph and initialize graph.
+//   - Wait for initialization (connecting to all bricks) to complete.
 //
 // Source: glfs.h
 func (v *Volume) Mount() error {
@@ -226,7 +226,7 @@ func (v *Volume) Mkdir(name string, perm os.FileMode) error {
 	return nil
 }
 
-// Removes an existing directory
+// Rmdir removes an existing directory
 //
 // Returns error on failure
 func (v *Volume) Rmdir(path string) error {
@@ -290,15 +290,18 @@ func (v *Volume) MkdirAll(path string, perm os.FileMode) error {
 	return nil
 }
 
-// RemoveAll removes path and any children it con
+// RemoveAll removes path and any children it contains
 
-// Open opens the named file on the the Volume v.
+// Open opens the named file on the Volume v.
 // The Volume must be mounted before calling Open.
 // Open is similar to os.Open in its functioning.
 //
 // name is the name of the file to be open.
 //
-// Returns a File object on success and a os.PathError on failure.
+// Returns a File object on success and an os.PathError on failure.
+//
+// BUG: Open does not support opening directories in Gluster v10.3+
+// as EISDIR is never returned as an error. Use OpenDir instead.
 func (v *Volume) Open(name string) (*File, error) {
 	isDir := false
 
@@ -320,7 +323,26 @@ func (v *Volume) Open(name string) (*File, error) {
 	return &File{name, Fd{cfd}, isDir}, nil
 }
 
-// OpenFile opens the named file on the the Volume v.
+// OpenDir opens the named directory on the Volume v.
+// The Volume must be mounted before calling Open.
+// Open is similar to os.Open in its functioning.
+//
+// name is the name of the directory to open.
+//
+// Returns a File object on success and an os.PathError on failure.
+func (v *Volume) OpenDir(name string) (*File, error) {
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+
+	cfd, err := C.glfs_opendir(v.fs, cname)
+	if cfd == nil {
+		return nil, &os.PathError{"opendir", name, err}
+	}
+
+	return &File{name, Fd{cfd}, true}, nil
+}
+
+// OpenFile opens the named file on the Volume v.
 // The Volume must be mounted before calling OpenFile.
 // OpenFile is similar to os.OpenFile in its functioning.
 //
@@ -328,7 +350,7 @@ func (v *Volume) Open(name string) (*File, error) {
 // flags is the access mode of the file.
 // perm is the permissions for the opened file.
 //
-// Returns a File object on success and a os.PathError on failure.
+// Returns a File object on success and an os.PathError on failure.
 //
 // BUG : perm is not used for opening the file.
 // NOTE: It is better to use Open, Create etc. instead of using OpenFile directly
@@ -377,18 +399,13 @@ func (v *Volume) Stat(name string) (os.FileInfo, error) {
 // Truncate changes the size of the named file
 //
 // Returns an error on failure
-//
-// TODO: gfapi currently (20131120) has not implement glfs_truncate.
-//       Once it has been implemented, renable the commented out code
-//       or write own function to implement the functionality of glfs_truncate
 func (v *Volume) Truncate(name string, size int64) error {
-	// cname := C.CString(name)
-	// defer C.free(unsafe.Pointer(cname))
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
 
-	// _, err := C.glfs_truncate(v.fs, cname, C.off_t(size))
+	_, err := C.glfs_truncate(v.fs, cname, C.off_t(size))
 
-	// return err
-	return errors.New("Truncate not implemented")
+	return err
 }
 
 // Rename a file or directory
@@ -409,7 +426,7 @@ func (v *Volume) Rename(oldpath string, newpath string) error {
 	return nil
 }
 
-// Get value of the extended attribute 'attr' and place it in 'dest'
+// Getxattr gets value of the extended attribute 'attr' and place it in 'dest'
 //
 // Returns number of bytes placed in 'dest' and error if any
 func (v *Volume) Getxattr(path string, attr string, dest []byte) (int64, error) {
@@ -436,7 +453,7 @@ func (v *Volume) Getxattr(path string, attr string, dest []byte) (int64, error) 
 	}
 }
 
-// Set extended attribute with key 'attr' and value 'data'
+// Setxattr sets extended attribute with key 'attr' and value 'data'
 //
 // Returns error on failure
 func (v *Volume) Setxattr(path string, attr string, data []byte, flags int) error {
@@ -457,7 +474,7 @@ func (v *Volume) Setxattr(path string, attr string, data []byte, flags int) erro
 	return err
 }
 
-// Remove extended attribute named 'attr'
+// Removexattr removes extended attribute named 'attr'
 //
 // Returns error on failure
 func (v *Volume) Removexattr(path string, attr string) error {
@@ -476,7 +493,7 @@ func (v *Volume) Removexattr(path string, attr string) error {
 	return err
 }
 
-// Get filesystem statistics
+// Statvfs get filesystem statistics
 //
 // Returns an error on failure
 func (v *Volume) Statvfs(path string, buf *Statvfs_t) error {
